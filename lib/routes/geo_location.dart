10f558063f4925a2466015b28abc2275572786bb
Krzysztof Piątkowski <57765358+Rautabout@users.noreply.github.com>
2021-11-28 23:32:34 +0100

    added marker and update marker functionality

--- lib/routes/geo_location.dart
+++ lib/routes/geo_location.dart
@@ -11,9 +11,10 @@ class GeoLocation extends StatefulWidget {
 
 //50.2844313,18.731742,17z
 class _GeoLocationState extends State<GeoLocation> {
+  List<Marker> marker=[];
   Completer<GoogleMapController> _controller = Completer();
-  double lat = 50.2844313;
-  double lng = 18.731742;
+  double lat = 50.2844673;
+  double lng = 18.7337511;
 
   static const CameraPosition _initialLocation = CameraPosition(
     target: LatLng(50.2844313, 18.731742),
@@ -21,10 +22,22 @@ class _GeoLocationState extends State<GeoLocation> {
   );
 
   static const CameraPosition _kLake = CameraPosition(
-      target: LatLng(50.2844313, 18.731742),
+      target: LatLng(50.2844673, 18.7337511),
       zoom: 18);
 
+  @override
+  void initState() {
+    marker.add(Marker(
+      markerId: MarkerId('Car Location'),
+      draggable: false,
+      position: LatLng(50.2844313, 18.731742),
+      onTap: (){
+        print("Marker tapped");
+      }
 
+    ));
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,7 +46,7 @@ class _GeoLocationState extends State<GeoLocation> {
         centerTitle: true,
         title: const Text('Geo Map Screen'),
         actions: [
-          IconButton(onPressed: _goToTheLake, icon: Icon(Icons.refresh))
+          IconButton(onPressed: _refreshLocation, icon: Icon(Icons.refresh))
         ],
       ),
       body: Center(
@@ -44,14 +57,26 @@ class _GeoLocationState extends State<GeoLocation> {
           onMapCreated: (GoogleMapController controller) {
             _controller.complete(controller);
           },
+          markers: Set.from(marker),
         ),
       ),
 
     );
   }
 
-  Future<void> _goToTheLake() async {
+  Future<void> _refreshLocation() async {
+    setState(() {
+      marker.clear();
+      marker.add(Marker(
+          markerId: MarkerId('Car Location'),
+          draggable: false,
+          position: LatLng(lat, lng),
+          onTap: (){
+            print("New marker tapped");
+          }
 
+      ));
+    });
     final GoogleMapController controller = await _controller.future;
     controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
   }
