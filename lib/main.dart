013bf99afb8ea5662ae870192ab841cd5c2a7d35
Krzysztof Piątkowski <57765358+Rautabout@users.noreply.github.com>
2022-01-15 23:49:51 +0100

    change to rtdb

--- lib/main.dart
+++ lib/main.dart
@@ -4,21 +4,34 @@ import 'package:flutter/material.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  //await Firebase.initializeApp(  );
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
+  final Future<FirebaseApp> _firebaseApp = Firebase.initializeApp();
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.blueGrey,
-      ),
-      home: NavRouter(),
+        theme: ThemeData(
+          primarySwatch: Colors.blueGrey,
+        ),
+        home: NavRouter(),
+        // home: FutureBuilder(
+        //   future: _firebaseApp,
+        //   builder: (context, snapshot) {
+        //     if (snapshot.hasError) {
+        //       print("error");
+        //     } else if (snapshot.hasData) {
+        //       return NavRouter();
+        //     } else {
+        //       return Center(child: CircularProgressIndicator());
+        //     }
+        //   },
+        // )
+        //NavRouter(),
 
-    );
+        );
   }
 }
